f7d0027a1a957c7ae823daf881ea02ee7747760a
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:40:35 -0300

    Lint ajust

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import {message, Input,Image, Typography, Descriptions,Button } from "antd";
-import { createGlobalStyle } from "styled-components";
+import React from 'react';
+import {message, Input,Image, Typography, Descriptions,Button } from 'antd';
+import { createGlobalStyle } from 'styled-components';
 import {useStateful} from 'react-hanger'
 
-import { ParentDiv } from "../components/ParentDiv";
-import { UsernameDiv } from "./../components/UsernameDiv";
+import { ParentDiv } from '../components/ParentDiv';
+import { UsernameDiv } from './../components/UsernameDiv';
 import { InstagramSearch } from '../controllers/InstagramSearch'
 import { SaveData } from '../controllers/SaveData'
-import {User} from "../interfaces/UserModel"
-import "typeface-poppins";
+import {User} from '../interfaces/UserModel'
+import 'typeface-poppins';
 import Logo from '../images/INSTOOL.png'
 import Icon from '../images/cubo.svg'
 
@@ -54,10 +54,10 @@ const App = () => {
             Entre com o nome de usuário do Instagram para obter informações...
           </Typography.Text>
           <Input.Search
-            className="username"
+            className='username'
             enterButton={<Button disabled={!canSearch()} type='primary' >Buscar</Button>}
-            addonBefore={"https://instagram.com/@"}
-            placeholder="seu_usuario"
+            addonBefore={'https://instagram.com/@'}
+            placeholder='seu_usuario'
             onSearch={onSearch}
             value={username.value}
             onChange={onUsernameChange}
@@ -66,19 +66,19 @@ const App = () => {
         {!!instagramData.value && (
           <React.Fragment>
             <img className='avatar' src={instagramData.value.profile_pic_url}/>
-            <Descriptions className="detalhes" bordered>
-              <Descriptions.Item label="Nome do usuário:">{instagramData.value.username}</Descriptions.Item>
-              <Descriptions.Item label="Nome completo:">{instagramData.value.full_name}</Descriptions.Item>
-              <Descriptions.Item label="Biografia:">{instagramData.value.biography || '[ vazio ]'}</Descriptions.Item>
-              <Descriptions.Item label="URL externa:">{instagramData.value.external_url}</Descriptions.Item>
-              <Descriptions.Item label="Conta recente:">{instagramData.value.is_joined_recently ? 'Sim' : 'Não'}</Descriptions.Item>
-              <Descriptions.Item label="É privado:">{instagramData.value.is_private ? 'Sim' : 'Não'}</Descriptions.Item>
-              <Descriptions.Item label="É um canal:">{instagramData.value.has_channel ? 'Sim' : 'Não'}</Descriptions.Item>
-              <Descriptions.Item label="É uma conta de negócio:">{instagramData.value.is_business_account ? 'Sim' : 'Não'}</Descriptions.Item>
-              <Descriptions.Item label="Categoria de negócio:">{instagramData.value.business_category_name || 'Não é um negócio'}</Descriptions.Item>
-              <Descriptions.Item label="Seguindo:">{instagramData.value.follow}</Descriptions.Item>
-              <Descriptions.Item label="Seguidores:">{instagramData.value.followed_by}</Descriptions.Item>
-              <Descriptions.Item label="Quantidade de posts:">{instagramData.value.timeline_media_count}</Descriptions.Item>
+            <Descriptions className='detalhes' bordered>
+              <Descriptions.Item label='Nome do usuário:'>{instagramData.value.username}</Descriptions.Item>
+              <Descriptions.Item label='Nome completo:'>{instagramData.value.full_name}</Descriptions.Item>
+              <Descriptions.Item label='Biografia:'>{instagramData.value.biography || '[ vazio ]'}</Descriptions.Item>
+              <Descriptions.Item label='URL externa:'>{instagramData.value.external_url}</Descriptions.Item>
+              <Descriptions.Item label='Conta recente:'>{instagramData.value.is_joined_recently ? 'Sim' : 'Não'}</Descriptions.Item>
+              <Descriptions.Item label='É privado:'>{instagramData.value.is_private ? 'Sim' : 'Não'}</Descriptions.Item>
+              <Descriptions.Item label='É um canal:'>{instagramData.value.has_channel ? 'Sim' : 'Não'}</Descriptions.Item>
+              <Descriptions.Item label='É uma conta de negócio:'>{instagramData.value.is_business_account ? 'Sim' : 'Não'}</Descriptions.Item>
+              <Descriptions.Item label='Categoria de negócio:'>{instagramData.value.business_category_name || 'Não é um negócio'}</Descriptions.Item>
+              <Descriptions.Item label='Seguindo:'>{instagramData.value.follow}</Descriptions.Item>
+              <Descriptions.Item label='Seguidores:'>{instagramData.value.followed_by}</Descriptions.Item>
+              <Descriptions.Item label='Quantidade de posts:'>{instagramData.value.timeline_media_count}</Descriptions.Item>
             </Descriptions>
             <Button onClick={onSave} type='primary' className='btnSave'>Salvar este usuário</Button>
           </React.Fragment>
