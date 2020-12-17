58bb2632a582980da0c97562135810b6a20bf9f6
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:40:13 -0300

    Lint ajust

--- src/controllers/InstagramSearch.ts
+++ src/controllers/InstagramSearch.ts
@@ -1,5 +1,5 @@
 import {api} from '../utils/api'
-import {User} from "../interfaces/UserModel"
+import {User} from '../interfaces/UserModel'
 
 export const InstagramSearch = async (username: string) => {
   return await api.get(`/instagram/${username}`) as User;
